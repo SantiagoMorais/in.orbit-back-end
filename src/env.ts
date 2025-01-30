d534d698a318de40f42067453337b39cbd9fe002
Felipe Santiago Morais <fillikz@gmail.com>
2025-01-30 09:21:31 -0300

    feat(env): add GitHub client ID and secret to environment variables

--- src/env.ts
+++ src/env.ts
@@ -1,11 +1,13 @@
 import z from "zod";
 
 const envSchema = z.object({
-    DATABASE_URL: z.string().url(),
-})
+  DATABASE_URL: z.string().url(),
+  GITHUB_CLIENT_ID: z.string(),
+  GITHUB_CLIENT_SECRET: z.string(),
+});
 
 const _env = envSchema.safeParse(process.env);
 
-if (!_env.success) throw new Error("Invalid Environment Variables ❌")
+if (!_env.success) throw new Error("Invalid Environment Variables ❌");
 
 export const env = _env.data;
