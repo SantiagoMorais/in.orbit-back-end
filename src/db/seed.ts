91d1a2844523fa1e150242a1e4cc9fb903ef105f
Felipe Santiago Morais <fillikz@gmail.com>
2025-01-30 09:43:32 -0300

    feat(seed): add user seeding and associate goals with user

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,18 +1,31 @@
 // Seed é importante para que quando alguém acessar nosso projeto, já tenha alguns itens adicionados no banco de dados
 import { client, db } from ".";
-import { goalCompletions, goals } from "./schema";
+import { goalCompletions, goals, users } from "./schema";
 import dayjs from "dayjs";
 
 const seed = async () => {
   await db.delete(goalCompletions);
   await db.delete(goals);
 
+  const [user] = await db
+    .insert(users)
+    .values({
+      name: "John Doe",
+      externalAccountId: 25849,
+      avatarUrl: "https://github.dev/SantiagoMorais.png",
+    })
+    .returning();
+
   const result = await db
     .insert(goals)
     .values([
-      { title: "Acordar cedo", desiredWeeklyFrequency: 5 },
-      { title: "Me exercitar", desiredWeeklyFrequency: 3 },
-      { title: "Fazer minhas orações", desiredWeeklyFrequency: 7 },
+      { userId: user.id, title: "Acordar cedo", desiredWeeklyFrequency: 5 },
+      { userId: user.id, title: "Me exercitar", desiredWeeklyFrequency: 3 },
+      {
+        userId: user.id,
+        title: "Fazer minhas orações",
+        desiredWeeklyFrequency: 7,
+      },
     ])
     .returning();
 
