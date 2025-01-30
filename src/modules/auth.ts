eb8f0a3759b216279021f926ad8e50b26abe4e8a
Felipe Santiago Morais <fillikz@gmail.com>
2025-01-30 12:11:53 -0300

    feat(auth): set protected header for JWT token generation

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -5,6 +5,7 @@ export const authenticateUser = async (userId: string) => {
   const secret = new TextEncoder().encode(env.JWT_SECRET);
 
   const token = await new SignJWT()
+    .setProtectedHeader({ alg: "HS256" })
     .setSubject(userId)
     .setExpirationTime("1d")
     .setIssuedAt()
